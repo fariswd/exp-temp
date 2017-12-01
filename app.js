2ba37bbcd23df8af3f3d56851437a5a413549108
Faris Widyantho <radenfaris@gmail.com>
2017-12-01 08:28:33 +0700

    update template using genrator

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()) // parse application/json
-app.use(morgan('tiny'))
+const index = require('./routes/indexRouter');
+const blog = require('./routes/blogRouter');
+
+const app = express();
+
 app.use(cors())
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//route variable
-const index = require('./routers/indexRouter')
-const blog = require('./routers/blogRouter')
+app.use('/', index);
+app.use('/blog', blog);
 
-//route use
-app.use('/', index)
-app.use('/api', blog)
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-//listen
-// app.listen(3000, () => {
-//   console.log('App running on port 3000!')
-// })
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app.listen(3000, () => {
-  console.log('App running on port 3000!')
-})
+  // render the error page
+  res.status(err.status || 500);
+  res.send({ err : 'error'});
+});
+
+module.exports = app;
